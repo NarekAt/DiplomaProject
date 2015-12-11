987dc0a77382085c37bff50b36609626caf15a13
NarekAt <unbeliever95@gmail.com>
2015-12-12 01:59:27 +0400

    Fixed bugs in connected_components_counter

--- central_place/unit_tests/alternate_property_computer_test/property_counters_test/connected_components_test.cpp
+++ central_place/unit_tests/alternate_property_computer_test/property_counters_test/connected_components_test.cpp
@@ -11,7 +11,56 @@ using namespace graph_types;
 namespace
 {
 
-TEST(ConnectedComponentsTest, xxx)
+TEST(ConnectedComponentsCounter, ShouldReturnItselfsForDisconnectedVertices)
+{
+    graph g(graph_size(5), storage_core_type::BITSETS_FULL);
+
+    ConnectedComponentsCounter::ResultCollection res = ConnectedComponentsCounter::calculate(g);
+    ASSERT_EQ(res.size(), 5u);
+
+    for (size_t i = 0; i < 5; ++i)
+    {
+        EXPECT_EQ(res[i], i);
+    }
+}
+
+TEST(ConnectedComponentsTest, ShouldTestOnGraphWithExactlyOneConnectedComponnent)
+{
+    graph g(graph_size(6), storage_core_type::BITSETS_FULL);
+    g.add_edge(0, 1);
+    g.add_edge(1, 2);
+    g.add_edge(2, 3);
+    g.add_edge(2, 4);
+    g.add_edge(4, 5);
+
+    ConnectedComponentsCounter::ResultCollection res = ConnectedComponentsCounter::calculate(g);
+
+    for (const auto& i : res)
+    {
+        EXPECT_EQ(i, 0);
+    }
+}
+
+TEST(ConnectedComponentsTest, ShouldCheckCaseWithConnectedBlockOfVerticesAndWithOneDisconnectedVertex)
+{
+    graph g(graph_size(7), storage_core_type::BITSETS_FULL);
+    g.add_edge(0, 1);
+    g.add_edge(1, 2);
+    g.add_edge(2, 3);
+    g.add_edge(2, 4);
+    g.add_edge(4, 5);
+
+    ConnectedComponentsCounter::ResultCollection res = ConnectedComponentsCounter::calculate(g);
+
+    for (size_t i = 0; i < 6; ++i)
+    {
+        EXPECT_EQ(res[i], 0);
+    }
+
+    EXPECT_EQ(res[6], 6);
+}
+
+TEST(ConnectedComponentsTest, ShouldCheckOnMultiComponentGraph)
 {
     graph g(graph_size(20), storage_core_type::BITSETS_FULL);
     g.add_edge(1, 2);
@@ -26,10 +75,30 @@ TEST(ConnectedComponentsTest, xxx)
     g.add_edge(13, 14);
     g.add_edge(15, 17);
     g.add_edge(17, 18);
-    g.add_edge(0, 16);
-    g.add_edge(16, 19);
 
     ConnectedComponentsCounter::ResultCollection res = ConnectedComponentsCounter::calculate(g);
+
+    // Component 1
+    for (size_t i = 1; i <= 5; ++i)
+        EXPECT_EQ(res[i], 1);
+    // Component 6
+    for (size_t i = 6; i <= 8; ++i)
+        EXPECT_EQ(res[i], 6);
+    // Component 9
+    for (size_t i = 9; i <= 12; ++i)
+        EXPECT_EQ(res[i], 9);
+    // Component 13
+    for (size_t i = 13; i <=14; ++i)
+        EXPECT_EQ(res[i], 13);
+    // Component 15
+        EXPECT_EQ(res[15], 15);
+        EXPECT_EQ(res[17], 15);
+        EXPECT_EQ(res[18], 15);
+
+    // Disconnected Components
+        EXPECT_EQ(res[0], 0);
+        EXPECT_EQ(res[16], 16);
+        EXPECT_EQ(res[19], 19);
 
     for (auto& r : res)
     {
