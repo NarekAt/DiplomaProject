4808ead953bb8250d6f6603d27c64c1685bb97c6
NarekAt <unbeliever95@gmail.com>
2015-12-19 13:17:11 +0400

    Implemented graph betweenness centrality parallel algorithm

--- central_place/alternate_property_computer/common/utility.h
+++ central_place/alternate_property_computer/common/utility.h
@@ -7,6 +7,19 @@
 
 #include "graph_types.h"
 
+#define MULTIPLE_TYPEDEF2(type, n1, n2) \
+    typedef type n1;                    \
+    typedef type n2
+
+#define MULTIPLE_TYPEDEF3(type, n1, n2, n3) \
+    MULTIPLE_TYPEDEF2(type, n1, n2);        \
+    typedef type n3
+
+#define MULTIPLE_TYPEDEF4(type, n1, n2, n3, n4) \
+    MULTIPLE_TYPEDEF3(type, n1, n2, n3);        \
+    typedef type n4
+
+
 namespace utility
 {
 
