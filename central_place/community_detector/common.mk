5cb16e3f75d7d7d469e3dae5704744674c5e31ce
NarekAt <unbeliever95@gmail.com>
2016-05-04 00:43:31 +0400

    External libs management has been done. Added new module in root folder: shared_libs

--- central_place/community_detector/common.mk
+++ central_place/community_detector/common.mk
@@ -2,14 +2,14 @@ GCC=mpic++
 
 ARCH = $(shell getconf LONG_BIT)
 ifeq ($(ARCH), 32)
-  XLLIBPATH =/home/natayan/libs_space/libxl-3.6.5.0/lib
+  XLLIBPATH =../../shared_libs/libxl-3.6.5.0/lib
 else
-  XLLIBPATH =/home/natayan/libs_space/libxl-3.6.5.0/lib64
+  XLLIBPATH =../../shared_libs/libxl-3.6.5.0/lib64
 endif
 
 CXXFLAGS=-std=c++11 -O3
 IFLAGS=
-LFLAGS=-lstdc++ -Wl,-Bstatic -lboost_serialization -lboost_graph -lboost_regex -lboost_system -lboost_filesystem -lboost_mpi -lboost_program_options -Wl,-Bdynamic -lm -L/home/natayan/boost/boost_1_53_0/stage/lib -lm -L/usr/local/lib -ligraph
+LFLAGS=-lstdc++ -Wl,-Bstatic -lboost_serialization -lboost_graph -lboost_regex -lboost_system -lboost_filesystem -lboost_mpi -lboost_program_options -Wl,-Bdynamic -lm -L$(BOOST_LIBS)
 LFLAGS+=-L$(XLLIBPATH) -lxl
 
 DIR=objs
---
+++ central_place/install/compile/load_external_libs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+(cd ../../shared_libs && tar -xvf libxl-3.6.5.0.tar.gz && tar -xvf OGDF.tar.gz)
